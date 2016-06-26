17bb581166522b9a2f2518d837a371483355951a
John  Mahr <john@johnhenrymahr.com>
2016-06-26 18:44:41 -0500

    updates to layout and styles

--- app/views/titleView.js
+++ app/views/titleView.js
@@ -1,6 +1,7 @@
 var View = require('app/views/_baseView')
 var conf = require('app/utils/_manifest').get('title')
 require('app/less/title.less')
+require('app/less/icons.less')
 if (conf) {
   module.exports = View.extend({
     template: require('app/dust/' + conf.template)
