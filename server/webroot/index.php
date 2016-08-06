41d50af42cc8c43e195d05259da4d11cc222f3e0
John  Mahr <john@johnhenrymahr.com>
2016-08-06 15:35:00 -0500

    begin working on basic index php file

--- server/webroot/index.php
+++ server/webroot/index.php
@@ -1,2 +1,9 @@
 <?php
-?>
+date_default_timezone_set('America/Chicago');
+define('SERVER_ROOT', dirname(realpath(__DIR__)) . "/");
+$server_name = filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_STRING);
+define('PROD', strpos($server_name, 'johnhenrymahr.com') !== false);
+require SERVER_ROOT . 'vendor/autoload.php';
+$graph = new \JHM\Graph();
+$assembler = $graph->get('Assembler');
+$dataProvider = $graph->get('DataProvider');
