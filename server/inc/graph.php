a4c80cd41fce94a6f87762c131ea17bab1752010
John  Mahr <john@johnhenrymahr.com>
2016-08-06 15:34:20 -0500

    finsh object graph class. Remove mergeSubstitutions method and enhance addRule method to provide same functionality and more. Fix test cases and mocking so testing graph is easier.

--- server/inc/graph.php
+++ server/inc/graph.php
@@ -2,69 +2,74 @@
 //https://github.com/Level-2/Dice
 namespace JHM;
 
-class Graph {
+class Graph
+{
 
-	private $Container;
+    private $Container;
 
-	protected $interfaceMap = [
-		'JHM\\CacheInterface' 				=> ['instance' => 'JHM\\FileCache'],	
-		'JHM\\ConfigInterface' 				=> ['instance' => 'JHM\\Config'],
-		'JHM\\DataProviderInterface' 		=> ['instance' => 'JHM\\DataProvider'],
-		'JHM\\FileLoaderInterface' 			=> ['instance' => 'JHM\\FileLoader'],
-		'JHM\\LoggerInterface' 				=> ['instance' => 'JHM\\Logger'],
-		'JHM\\ManifestInterface' 			=> ['instance' => 'JHM\\Manifest'],
-		'JHM\\RendererInterface' 			=> ['instance' => 'JHM\\Renderer'],
-		'JHM\\TemplateFactoryInterface' 	=> ['instance' => 'JHM\\TemplateFactory']
-	];
+    protected $interfaceMap = [
+        'JHM\\CacheInterface' => ['instance' => 'JHM\\FileCache'],
+        'JHM\\ConfigInterface' => ['instance' => 'JHM\\Config'],
+        'JHM\\DataProviderInterface' => ['instance' => 'JHM\\DataProvider'],
+        'JHM\\FileLoaderInterface' => ['instance' => 'JHM\\FileLoader'],
+        'JHM\\LoggerInterface' => ['instance' => 'JHM\\Logger'],
+        'JHM\\ManifestInterface' => ['instance' => 'JHM\\Manifest'],
+        'JHM\\RendererInterface' => ['instance' => 'JHM\\Renderer'],
+        'JHM\\TemplateFactoryInterface' => ['instance' => 'JHM\\TemplateFactory'],
+    ];
 
-	protected $rules = [];
+    protected $rules = [];
 
-	protected function _getFQN($className, $ns = 'JHM') {
-		$ns = $ns.'\\';
-		$fqn = $ns.str_replace($ns, '', $className);
+    protected function _getFQN($className, $ns = 'JHM')
+    {
+        if ($className === '*') {
+            return $className;
+        }
+        $ns = $ns . '\\';
+        $fqn = $ns . str_replace($ns, '', $className);
 
-		return $fqn;
-	}
+        return $fqn;
+    }
 
-	public function __construct() {
+    public function __construct()
+    {
 
-		$this->Container = new \Dice\Dice();
-		$this->rules['*'] = [
-			"shared" => true,
-			"substitutions" => $this->interfaceMap
-		];
-		$this->Container->addRule('*', $this->rules['*']);
-	}
+        $this->Container = new \Dice\Dice();
+        $this->rules['*'] = [
+            "shared" => true,
+            "substitutions" => $this->interfaceMap,
+        ];
+        $this->Container->addRule('*', $this->rules['*']);
+    }
 
-	public function addRule($className, array $rule=[]) {
-		$fqn = $this->_getFQN($className);
-		if (class_exists($fqn)) {
-			if (!empty($rule)) {
-				$this->rules[$fqn] = $rule;
-				$this->Container->addRule($fqn, $this->rules[$fqn]);
-			}
-		}
-	}
+    public function addRule($className, array $rule = [], array $objects = [])
+    {
+        $fqn = $this->_getFQN($className);
+        if (class_exists($fqn) || $fqn === '*') {
+            if (empty($rule)) {
+                $rule = $this->rules['*'];
+            }
+            if (!empty($objects)) {
+                $keys = array_keys($objects);
+                foreach ($keys as $key) {
+                    if (!array_key_exists($key, $rule)) {
+                        $rule[$key] = [];
+                    }
+                    $rule[$key] = array_merge($rule[$key], $objects[$key]);
+                }
+            }
+            $this->rules[$fqn] = $rule;
+            $this->Container->addRule($fqn, $this->rules[$fqn]);
+        }
+    }
 
-	public function mergeSubstitution($className, array $map=[]) {
-		$fqn = $this->_getFQN($className);
-		if (class_exists($fqn) && 
-			!empty($map))
-		{
-			$this->rules[$fqn] = [
-				"shared" => true,
-				"substitutions" => array_merge($this->interfaceMap, $map)
-			];
-			$this->Container->addRule($fqn, $this->rules[$fqn]);
-		}		
-	}
-
-	public function get($className) {
-		$fqn = $this->_getFQN($className);
-		if (class_exists($fqn)) {
-			return $this->Container->create($fqn);
-		}
-		return false;
-	}
+    public function get($className, $default = false)
+    {
+        $fqn = $this->_getFQN($className);
+        if (class_exists($fqn)) {
+            return $this->Container->create($fqn);
+        }
+        return $default;
+    }
 
 }
--- server/tests/GraphTest.php
+++ server/tests/GraphTest.php
@@ -1,36 +1,76 @@
 <?php
-class GraphTest extends \PHPUnit\Framework\TestCase {
-	
-	protected $obj;
+class GraphTest extends \PHPUnit\Framework\TestCase
+{
 
-	protected function setUp() {
+    protected $obj;
 
-		$this->obj = new \JHM\Graph();
-	}
+    protected function setUp()
+    {
+        $this->obj = new \JHM\Graph();
+    }
 
-	protected function mockDeps($className, $interface, $mockInstance) {
-		$map =  [
-				$interface => [
-					"instance" => function() use ($mockInstance) {
-						return $mockInstance;
-					}
-				]		
-			];
-		$this->obj->mergeSubstitution($className, $map);
-	}
+    protected function tearDown()
+    {
+        \Mockery::close();
+    }
 
-	public function testAssemberGet() {
-		$dataPath = realpath(__DIR__) . '/data/mockManifest.json';
+    protected function mockDeps($className, $interface, $property, \Mockery\MockInterface $mock)
+    {
+
+        $map = [
+            $interface => [
+                "instance" => function () use ($mock) {
+                    return $mock;
+                },
+            ],
+        ];
+
+        $this->obj->addRule($className, [], [$property => $map]);
+
+    }
+
+    public function testAssemberGet()
+    {
+        $dataPath = realpath(__DIR__) . '/data/mockManifest.json';
+        $config = [
+            "testconfig" => [
+                "flags" => [
+                    "loggingEnabled" => false,
+                ],
+            ],
+        ];
         $json = json_decode(file_get_contents($dataPath), true);
         if (!is_array($json)) {
             throw new Exception('Manifest Test: data not ready. Data path: ' . $dataPath);
         }
-        $fileLoaderMock = \Mockery::mock('\JHM\FileLoaderInterface');
-        $fileLoaderMock->shouldReceive('load')->with('manifest.json', true)->once()->andReturn($json);
-        $this->mockDeps('Assembler', 'JHM\FileLoaderInterface', $fileLoaderMock);	
-		$assembler = $this->obj->get('Assembler');
-		$this->AssertEquals('Assembler', get_class($assembler));
-	}
+        $fileLoaderMock = \Mockery::mock('JHM\\FileLoaderInterface')
+            ->shouldReceive('load')
+            ->with('manifest.json', true)
+            ->once()
+            ->andReturn($json)
+            ->getMock();
+
+        $this->mockDeps('Manifest', 'JHM\\FileLoaderInterface', 'substitutions', $fileLoaderMock);
+        $this->obj->addRule('Logger', [], ['constructParams' => ['testconfig', $config]]);
+        $assembler = $this->obj->get('Assembler');
+        $this->AssertInstanceOf(\JHM\Assembler::class, $assembler);
+        $clone = (array) $assembler;
+        $this->assertEquals("\0*\0manifest", array_Keys($clone)[0]); // protected propertie keys get prepended with \0*\0 when cast to Array, private \0Class_name\0
+        $this->assertInstanceOf(\JHM\TemplateFactory::class, $clone["\0*\0templateFactory"]);
+        $this->assertInstanceOf(\JHM\Manifest::class, $clone["\0*\0manifest"]);
+    }
+
+    public function testDataProviderGet()
+    {
+        $fileLoaderMock = \Mockery::mock('JHM\\FileLoaderInterface');
+        $loggerMock = \Mockery::mock('JHM\\LoggerInterface');
+        $this->mockDeps('Config', 'JHM\\FileLoaderInterface', 'substitutions', $fileLoaderMock);
+        $this->mockDeps('Config', 'JHM\\LoggerInterface', 'substitutions', $loggerMock);
+        $dataProvider = $this->obj->get('DataProvider');
+        $this->assertInstanceOf(\JHM\DataProvider::class, $dataProvider);
+        $clone = (array) $dataProvider;
+        $this->assertInstanceOf(\JHM\Logger::class, $clone["\0*\0logger"]);
+        $this->assertInstanceOf(\JHM\fileLoader::class, $clone["\0*\0fileLoader"]);
+    }
 
 }
-
