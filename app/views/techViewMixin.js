d0687f701e2be3d7e58f3513f1e966db74f59b79
John Mahr <john@johnhenrymahr.com>
2017-04-04 20:41:28 -0500

    replace tech list popover with bs modal. Remove popover handler from view. Move image inside a and add a span for main label. Add labelledby attribute to li.

--- app/views/techViewMixin.js
+++ app/views/techViewMixin.js
@@ -1,24 +1,3 @@
-var $ = require('jquery')
 module.exports = {
-  onAttach: function () {
-    $('body').bind('click', function (e) {
-      $('.tech__popover').popover('hide')
-    })
-    this.$('.tech__popover').each(function (idx, ele) {
-      var $ele = $(ele)
-      var $content = $ele.next('.tech__item-content')
-      $ele
-      .on('click', function (e) {
-        e.preventDefault()
-        e.stopPropagation()
-      })
-      .popover({
-        content: $('.tech__item-content--body', $content).html(),
-        title: $('.tech__item-content--title', $content).text(),
-        container: $ele.parents('.tech__item'),
-        placement: 'top',
-        html: true
-      })
-    })
-  }
+  onAttach: function () {}
 }
