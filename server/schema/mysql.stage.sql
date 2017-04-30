68a558e71154c08e9154f76890864c9d4869d2b3
John  Mahr <john@johnhenrymahr.com>
2017-04-30 14:43:33 -0500

    add mysql schema defs

---
+++ server/schema/mysql.stage.sql
@@ -0,0 +1,42 @@
+# Dump of table stage_contact
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `stage_contact` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `company` varchar(255) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table stage_download
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `stage_download` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(11) NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `token` varchar(255) NOT NULL DEFAULT '',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table stage_message
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `stage_message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `topic` varchar(255) DEFAULT NULL,
+  `message` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
