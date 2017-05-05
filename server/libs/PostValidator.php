8b55e8f6c447fb22ecfb68892fe074140b58c382
John Mahr <john.mahr@bestbuy.com>
2017-05-05 16:22:34 -0500

    move api handler validation functionality into an abstract class for reusability

---
+++ server/libs/PostValidator.php
@@ -0,0 +1,59 @@
+<?php
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+namespace JHM;
+
+abstract class PostValidator {
+    
+    protected $requiredFields = [];
+
+    protected $honeyPotField = '';
+
+    protected function _validate(ParameterBag $request)
+    {
+        $keys = $request->keys();
+        if (empty($keys)) {
+          return false;
+        }
+        
+        $diff = array_diff($this->requiredFields, $keys);
+
+        if (!empty($diff)) {
+            return false;
+        }
+        
+        $honeyPot = (empty($this->honeyPotField)) ? false : $this->honeyPotField;
+        
+        foreach ($keys as $key) {
+          if (strpos(strtolower($key), 'email') !== false && $request->get($key) === false)  {
+            return false;
+          }
+          if (in_array($key, $this->requiredFields) && empty($request->get($key))) {
+            return false; 
+          }
+          if ($honeyPot && $key === $honeyPot && !empty($request->get($honeyPot))) {
+            return false;
+          }
+        }
+
+        return true;
+    }
+
+    /**
+    * validate email address
+    * @access private
+    * @param email string
+    * @return boolean valid
+    */
+    private function _validateEmail($email) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+        $email_domain = preg_replace('/^.+?@/', '', $email).'.';
+        if(!checkdnsrr($email_domain, 'MX') && !checkdnsrr($email_domain, 'A')){
+           return false;
+        }
+        return true;
+    }
+}
