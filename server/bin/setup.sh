27a0586b79d9a4e8ffdd11c9b1bb14183aa51817
John  Mahr <john@johnhenrymahr.com>
2017-08-13 16:38:42 -0500

    add mail digest folder to setup script

--- server/bin/setup.sh
+++ server/bin/setup.sh
@@ -7,11 +7,12 @@ fi
 if [ ! -d ./storage/logs ]; then
   printf "Create log directory\n"
   mkdir ./storage/logs
+  chmod 777 ./storage/logs
 fi
-if [ ! -f ./storage/logs/{{logfile}} ]; then
-   printf "Create system log\n"
-   touch ./storage/logs/{{logfile}}
-   chmod 666 ./storage/logs/{{logfile}}
+if [ ! -d ./storage/digest ]; then
+  printf "Create digest directory\n"
+  mkdir ./storage/digest
+  chmod 777 ./storage/digest
 fi
 if [ -d ./storage/cache ]; then
   printf "Removing old cache directory\n"
