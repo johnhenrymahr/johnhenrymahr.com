f230c04477aa6eb92b2ecb9e96dc070d76e03510
John Mahr <john.mahr@bestbuy.com>
2017-08-14 10:54:59 -0500

    add logging message to exception blocks to log exception message. Add a environmentTest file.

--- server/webroot/download/index.php
+++ server/webroot/download/index.php
@@ -5,11 +5,16 @@ define('INCLUDES', APP_PATH . 'includes/');
 require APP_PATH . 'vendor/autoload.php';
 $graph = new \JHM\Graph();
 $downloadHandler = $graph->get('DownloadHandler');
+$logger $graph->get('Logger');
 $api = $graph->get('Api');
 try {
     $api->defaultHandler($downloadHandler);
     $api->init();
     $api->respond();
+} catch (\JHM\JhmException $e) {
+		$logger->log('ERROR', 'Download hanlder threw a fatal JHM Exception', ['exception' => $e->getMessage()])
+    http_response_code('502');    
 } catch (Exception $e) {
+		$logger->log('ERROR', 'Download hanlder threw a fatal Generic Exception', ['exception' => $e->getMessage()])
     http_response_code('502'); // bad gateway
 }
---
+++ server/webroot/environmentTest.php
@@ -0,0 +1,68 @@
+<?php
+/*
+this file is a functional test to be run on a particular enviroment. It tests things like sending mail and database interactions to make sure they are working. It should probably be deleted after it is run successfully.
+*/
+
+ini_set('display_errors', 1);
+date_default_timezone_set('America/Chicago');
+define('APP_PATH', realpath('{{serverApp}}') . '/');
+define('INCLUDES', APP_PATH . 'includes/');
+require APP_PATH . 'vendor/autoload.php';
+$graph = new \JHM\Graph();
+$contactStorage = $graph->get('ContactStorage');
+
+$email = 'testemail@mail.com';
+$name = 'Joe Testman';
+$company = 'MFC Corp';
+$phone = '6128789898';
+
+echo '<h1>Environment Test: '.gethostname().'</h1>';
+
+if ($contactStorage->isReady()) {
+
+	echo '<h2>Contact Storage Class</h2>';
+
+	echo '<h3>Creating contact record</h3>';
+
+	$cid = $contactStorage->addContact($email, $name, $company, $phone);
+
+	if ($cid) {
+		echo "<p><strong>OK</strong> ID: {$result}</p>";
+		echo '<h3>Adding a messge</h3>';
+		$mid = $contactStorage->addMessage($cid, 'Environment Test', 'Adding a message to see if it works');
+		if ($mid) {
+			echo '<p><strong>OK</strong></p>';			
+		} else {
+			echo '<p>Add message failed</p>';
+		}
+		echo '<h2>Create download token</h2>';
+		$token = $contactStorage->addDownloadRecord($cid, $email, 'testfile1', 'image/jpeg');
+		if ($token) {
+			echo '<p><strong>OK</strong></p>';
+			var_dump($token);
+			echo '<h3>Validate Token</h3>';
+			$validateToken = $contactStorage->validateDownloadToken($token['token']);
+			if ($validateToken) {
+				echo '<p><strong>OK</strong></p>';
+				var_dump($validateToken);
+			}
+			echo '<h3>Delete Token</h3>';
+			$deleteToken = $contactStorage->removeDownloadToken($token['token']);
+			if ($deleteToken) {
+				echo '<p><strong>OK</strong></p>';
+				var_dump($deleteToken);
+			}
+		} else {
+			echo '<p>add download token failed</p>';
+		}
+
+	} else {
+		echo '<p>Add contact record failed</p>';
+	}	
+
+} else {
+	echo '<p>Storage not ready on this server</p>';
+}
+
+
+
