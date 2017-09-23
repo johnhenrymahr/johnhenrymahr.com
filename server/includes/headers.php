5473c9b444ee8a82de2c8c19c7d865460c90ebac
John  Mahr <john@johnhenrymahr.com>
2017-09-23 12:03:26 -0500

    add a basic content security policy header (csp) to the main document response.

--- server/includes/headers.php
+++ server/includes/headers.php
@@ -5,6 +5,16 @@ header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 header('Content-Type: text/html; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('X-Frame-Options: DENY'); //prevent clickjacking
+if (defined('INCLUDES')) {
+    // add a basic content security policy
+    // @url https://content-security-policy.com
+    $csp = @file_get_contents(INCLUDES . 'csp.txt');
+    if ($csp) {
+        header("Content-Security-Policy: {$csp}");
+        header("X-Content-Security-Policy: {$csp}");
+        header("X-WebKit-CSP: {$csp}");
+    }
+}
 if (IS_PROD) {
     // implement hsts header in prod -- force browser to use https
     // https://www.owasp.org/index.php/HTTP_Strict_Transport_Security_Cheat_Sheet
