33802c4ffdf7b0ad59a2c15eea21c57b71d21f31
John Mahr <john.mahr@bestbuy.com>
2017-10-23 11:47:16 -0500

    add a manual session lifespan validation to session class.

--- server/libs/Session.php
+++ server/libs/Session.php
@@ -6,9 +6,16 @@ class Session implements SessionInterface
 
     private $started = false;
 
+    private $lifespan = 1440;
+
     public function __construct()
     {
         $this->started = session_status() === PHP_SESSION_ACTIVE;
+        $system_timeout = @ini_get('session.gc_maxlifetime');
+        if ($system_timeout && is_int($system_timeout)) {
+            $this->lifespan = $system_timeout;
+        }
+        $this->_valdiate();
     }
 
     public function start()
@@ -38,5 +45,19 @@ class Session implements SessionInterface
         if ($this->started) {
             $_SESSION[$key] = $val;
         }
+    }
+
+    protected function _validate () 
+    {
+        if ($this->started) {
+            if (isset($_SESSION['LAST_ACTIVITY']) && 
+                (time() - $_SESSION['LAST_ACTIVITY'] > $this->lifespan)) {
+                $_SESSION = array();  
+                session_destroy();   // destroy session data in storage
+                $this->started = false;
+            }
+        } else {
+            $_SESSION['LAST_ACTIVITY'] = time();
+        } 
     }
 }
