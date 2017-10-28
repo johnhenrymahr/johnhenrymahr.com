96587e9764e5e72730d4e7fb3ed335ee136953f2
John Mahr <john@johnhenrymahr.com>
2017-10-28 09:28:02 -0500

    fix flow logic bug with session validation method

--- server/libs/Session.php
+++ server/libs/Session.php
@@ -1,63 +1,56 @@
 <?php
 namespace JHM;
 
-class Session implements SessionInterface
-{
+class Session implements SessionInterface {
 
-    private $started = false;
+	private $started = false;
 
-    private $lifespan = 1440;
+	private $lifespan = 1440;
 
-    public function __construct()
-    {
-        $this->started = session_status() === PHP_SESSION_ACTIVE;
-        $system_timeout = @ini_get('session.gc_maxlifetime');
-        if ($system_timeout && is_int($system_timeout)) {
-            $this->lifespan = $system_timeout;
-        }
-        $this->_valdiate();
-    }
+	public function __construct() {
+		$this->started = session_status() === PHP_SESSION_ACTIVE;
+		$system_timeout = @ini_get('session.gc_maxlifetime');
+		if ($system_timeout && is_int($system_timeout)) {
+			$this->lifespan = $system_timeout;
+		}
+		$this->_valdiate();
+	}
 
-    public function start()
-    {
-        if (!$this->started) {
-            $this->started = session_start();
-        }
-        return $this->started;
-    }
+	public function start() {
+		if (!$this->started) {
+			$this->started = session_start();
+		}
+		return $this->started;
+	}
 
-    public function id()
-    {
-        if ($this->started) {
-            return session_id();
-        }
-    }
+	public function id() {
+		if ($this->started) {
+			return session_id();
+		}
+	}
 
-    public function get($key)
-    {
-        if ($this->started && isset($_SESSION[$key])) {
-            return $_SESSION[$key];
-        }
-    }
+	public function get($key) {
+		if ($this->started && isset($_SESSION[$key])) {
+			return $_SESSION[$key];
+		}
+	}
 
-    public function set($key, $val)
-    {
-        if ($this->started) {
-            $_SESSION[$key] = $val;
-        }
-    }
+	public function set($key, $val) {
+		if ($this->started) {
+			$_SESSION[$key] = $val;
+		}
+	}
 
-    protected function _validate () 
-    {
-        if ($this->started) {
-            if (isset($_SESSION['LAST_ACTIVITY']) && 
-                (time() - $_SESSION['LAST_ACTIVITY'] > $this->lifespan)) {
-                $_SESSION = array();  
-                session_destroy();   // destroy session data in storage
-                $this->started = false;
-            }
-        } else {
-            $_SESSION['LAST_ACTIVITY'] = time();
-        } 
-    }
+	protected function _validate() {
+		if ($this->started) {
+			if (isset($_SESSION['LAST_ACTIVITY']) &&
+				(time() - $_SESSION['LAST_ACTIVITY'] > $this->lifespan)) {
+				$_SESSION = array();
+				session_destroy(); // destroy session data in storage
+				$this->started = false;
+			} else {
+				$_SESSION['LAST_ACTIVITY'] = time();
+			}
+		}
+	}
 }
