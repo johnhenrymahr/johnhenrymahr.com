1815c67f84fe386f6dec2745cfe04b6caf0fe2f0
John Mahr <john@johnhenrymahr.com>
2017-10-28 09:29:15 -0500

    add logging to CsrfToken class to track invalid tokens and failed validations. Update tests with logger mock expectations.

--- server/libs/CsrfToken.php
+++ server/libs/CsrfToken.php
@@ -1,53 +1,56 @@
 <?php
 namespace JHM;
 
-class CsrfToken implements CsrfTokenInterface
-{
+class CsrfToken implements CsrfTokenInterface {
 
-    private $key;
+	private $key;
 
-    private $fieldId = 'pr_id';
+	private $fieldId = 'pr_id';
 
-    protected $ready = false;
+	protected $ready = false;
 
-    protected $session;
+	protected $logger;
 
-    public function __construct(SessionInterface $session, $key = '')
-    {
-        $this->session = $session;
-        $this->ready = $this->session->start();
-        if ($this->ready) {
-            $sKey = $this->session->get('csrfKey');
-            if ($sKey) {
-                $this->key = $sKey;
-            } else {
-                $this->key = (empty($key)) ? bin2hex(openssl_random_pseudo_bytes(32)) : $key;
-                $this->session->set('csrfKey', $this->key);
-            }
-        }
-    }
+	protected $session;
 
-    public function getField()
-    {
-        return $this->fieldId;
-    }
+	public function __construct(SessionInterface $session, LoggerInterface $logger, $key = '') {
+		$this->logger = $logger;
+		$this->session = $session;
+		$this->ready = $this->session->start();
+		if ($this->ready) {
+			$sKey = $this->session->get('csrfKey');
+			if ($sKey) {
+				$this->key = $sKey;
+			} else {
+				$this->key = (empty($key)) ? bin2hex(openssl_random_pseudo_bytes(32)) : $key;
+				$this->session->set('csrfKey', $this->key);
+			}
+		}
+	}
 
-    public function generateToken($formId)
-    {
-        if ($this->ready === false) {
-            throw new JhmException('CSRF Not Ready.');
-        }
-        $id = $this->session->id();
-        return hash_hmac('sha256', $id . trim($formId), $this->key);
-    }
+	public function getField() {
+		return $this->fieldId;
+	}
 
-    public function checkToken($token, $formId)
-    {
-        if (!is_string($token)) {
-            return false;
-        }
+	public function generateToken($formId) {
+		if ($this->ready === false) {
+			throw new JhmException('CSRF Not Ready.');
+		}
+		$id = $this->session->id();
+		return hash_hmac('sha256', $id . trim($formId), $this->key);
+	}
 
-        return $token === $this->generateToken($formId);
-    }
+	public function checkToken($token, $formId) {
+		if (!is_string($token)) {
+			$this->logger->log('WARNING', 'CSRF: invalid token format');
+			return false;
+		}
+
+		$isValid = $token === $this->generateToken($formId);
+		if (!$isValid) {
+			$this->logger->log('WARNING', 'CSRF: invalid token.', array('token' => $token, 'formId' => $formId));
+		}
+		return $isValid;
+	}
 
 }
--- server/tests/CrsfTokenTest.php
+++ server/tests/CrsfTokenTest.php
@@ -4,6 +4,8 @@ class CrsfTokenTest extends \PHPUnit\Framework\TestCase
 {
     protected $sessionMock;
 
+    protected $loggerMock;
+
     protected $obj;
 
     protected $token;
@@ -15,8 +17,9 @@ class CrsfTokenTest extends \PHPUnit\Framework\TestCase
         $this->sessionMock->shouldReceive('set')->with('csrfKey', 'testkey')->byDefault();
         $this->sessionMock->shouldReceive('id')->andReturn('theid');
         $this->sessionMock->shouldReceive('get')->with('csrfKey')->andReturn(null)->byDefault();
+        $this->loggerMock = \Mockery::mock('\JHM\LoggerInterface');
         $this->token = hash_hmac('sha256', 'theidfoo', 'testkey');
-        $this->obj = new \JHM\CsrfToken($this->sessionMock, 'testkey');
+        $this->obj = new \JHM\CsrfToken($this->sessionMock, $this->loggerMock, 'testkey');
     }
 
     protected function tearDown()
@@ -43,12 +46,14 @@ class CrsfTokenTest extends \PHPUnit\Framework\TestCase
     public function testCheckTokenFail()
     {
         $this->sessionMock->shouldReceive('get')->with('csrfKey')->andReturn('testkey');
+        $this->loggerMock->shouldReceive('log')->with('WARNING', 'CSRF: invalid token.', array('token' => 'wrong-token', 'formId' => 'foo'));
         $this->assertFalse($this->obj->checkToken('wrong-token', 'foo'));
     }
 
     public function testCheckInvalidTokenType()
     {
         $this->sessionMock->shouldReceive('get')->with('csrfKey')->andReturn('testkey');
+        $this->loggerMock->shouldReceive('log')->with('WARNING', 'CSRF: invalid token format');
         $this->assertFalse($this->obj->checkToken(null, 'foo'));
     }
 
@@ -56,7 +61,7 @@ class CrsfTokenTest extends \PHPUnit\Framework\TestCase
     {
         $this->expectException(\JHM\JhmException::class);
         $this->sessionMock->shouldReceive('start')->andReturn(false);
-        $obj = new \JHM\CsrfToken($this->sessionMock);
+        $obj = new \JHM\CsrfToken($this->sessionMock, $this->loggerMock);
         $obj->generateToken('foobar');
 
     }
