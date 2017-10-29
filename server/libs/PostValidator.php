cdb27538bf78b2e476c625b78af55a32afad3a6e
John  Mahr <john@johnhenrymahr.com>
2017-10-29 14:57:38 -0500

    add additional error logging to post validator

--- server/libs/PostValidator.php
+++ server/libs/PostValidator.php
@@ -13,6 +13,13 @@ abstract class PostValidator
 
     protected $csrfField = 'pr_id';
 
+    protected function _validatorLogger($message, $context = array(), $level = 'WARNING')
+    {
+        if (property_exists($this, 'logger') && method_exists($this->logger, 'log')) {
+            $this->logger->log($level, $message, $context);
+        }
+    }
+
     protected function _validate(ParameterBag $request, $formId = '')
     {
 
@@ -24,12 +31,14 @@ abstract class PostValidator
 
         $keys = $request->keys();
         if (empty($keys)) {
+            $this->_validatorLogger('Field Validation: no fields found');
             return false;
         }
 
         $diff = array_diff($this->requiredFields, $keys);
 
         if (!empty($diff)) {
+            $this->_validatorLogger('Field Validation: required fields missing.', $diff);
             return false;
         }
 
@@ -37,12 +46,15 @@ abstract class PostValidator
 
         foreach ($keys as $key) {
             if (strpos(strtolower($key), 'email') !== false && $this->_validateEmail($request->get($key)) === false) {
+                $this->_validatorLogger('Email Validation: no email address');
                 return false;
             }
             if (in_array($key, $this->requiredFields) && empty($request->get($key))) {
+                $this->_validatorLogger('Field Validation: required field empty: ' . $key);
                 return false;
             }
             if ($honeyPot && $key === $honeyPot && !empty($request->get($honeyPot))) {
+                $this->_validatorLogger('Field Validation: honey pot field present: ' . $key);
                 return false;
             }
         }
@@ -59,10 +71,12 @@ abstract class PostValidator
     private function _validateEmail($email)
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->_validatorLogger('Email Validation: invalid address', array('address' => $email));
             return false;
         }
         $email_domain = preg_replace('/^.+?@/', '', $email) . '.';
         if (function_exists('checkdnsrr') && !checkdnsrr($email_domain, 'MX') && !checkdnsrr($email_domain, 'A')) {
+            $this->_validatorLogger('Email Validation: No DNS record found for address', array('address' => $email, 'domain' => $email_domain));
             return false;
         }
         return true;
